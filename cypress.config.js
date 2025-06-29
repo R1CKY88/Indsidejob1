bfbc01450d499066319dce69b1061b3a364c9599
R1CKY88 <franciskussanjaya@gmail.com>
2025-06-29 13:58:30 +0700

    Delete cypress.config.js

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(off, config) {
-      // implement node event listeners here
-    },
-  },
-});

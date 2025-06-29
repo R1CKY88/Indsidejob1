18945dc7ceff0c050b333d5783e046eca545296d
Ricky <franciskussanjaya@.com>
2025-06-29 14:11:19 +0700

    inisiasi project

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(off, config) {
+    setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
   },
 });
